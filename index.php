1ffe008c041c2a9fbd24ea63a3afad2fb5eb3cba
spmuthu21 <muthu@globalensolutions.com>
2016-11-08 15:10:01 +0530

    changed template file integrated by muthu

--- index.php
+++ index.php
@@ -190,7 +190,7 @@ if (defined('ENVIRONMENT'))
 
 		define('APPPATH', BASEPATH.$application_folder.'/');
 	}
-
+	define('APP_URL', ($_SERVER['SERVER_PORT'] == 443 ? 'https' : 'http') . "://{$_SERVER['SERVER_NAME']}".str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']));
 /*
  * --------------------------------------------------------------------
  * LOAD THE BOOTSTRAP FILE
