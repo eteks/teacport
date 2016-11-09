fa0e979d68cbfa273643f46f8260397b393325f2
thangamariappanp <thangamgold45@gmail.com>
2016-11-09 19:55:14 +0530

    Server side validation for login and registration

--- application/controllers/job_provider.php
+++ application/controllers/job_provider.php
@@ -1,6 +1,14 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Job_provider extends CI_Controller {
+
+	public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('ajax_model'); 
+        $this->load->library('form_validation'); 
+        $this->load->library('session'); 
+    }
 	public function index()
 	{
 		$this->load->view('job-providers-login');
@@ -9,4 +17,14 @@ class Job_provider extends CI_Controller {
 	{
 		$this->load->view('register-job-providers');
 	}
+	public function register_job_provider()
+	{
+		$data = $this->ajax_model->get_registeration_status_provider();
+		echo $data;
+	}
+	    public function login_job_provider()
+    {   
+        $data = $this->ajax_model->get_login_status_provider();
+        echo $data;
+    }
 }
--- application/controllers/job_seeker.php
+++ application/controllers/job_seeker.php
@@ -1,12 +1,30 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Job_seeker extends CI_Controller {
+	public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('ajax_model'); 
+        $this->load->library('form_validation'); 
+        // $this->load->library('session'); 
+    }
+
 	public function index()
 	{
 		$this->load->view('job-seekers-login');
 	}
 	public function signup()
 	{
-		$this->load->view('register-job-seekers.php');
+		$this->load->view('register-job-seekers');
 	}
+	public function register_job_seeker()
+	{
+		$data = $this->ajax_model->get_registeration_status_seeker();
+		echo $data;
+	}
+	public function login_job_seeker()
+    {   
+        $data = $this->ajax_model->get_login_status_seeker();
+        echo $data;
+    }
 }
