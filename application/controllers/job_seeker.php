2ffaa435224ce3bdc44f0d7dc2800ddc99d4e919
thangamariappanp <thangamgold45@gmail.com>
2016-11-16 10:18:22 +0530

    Fixed issues on form val

--- application/controllers/job_seeker.php
+++ application/controllers/job_seeker.php
@@ -72,11 +72,13 @@ class Job_seeker extends CI_Controller {
 		}
 		/* Registration page loading with posted data */
 		else{
+			$this->form_validation->set_error_delimiters('<div class="error">', '</div>'); // Displaying Errors in Div
 			/* Set validate condition for registration form */
 			$this->form_validation->set_rules('candidate_institution_type', 'Institution', 'trim|required|numeric|xss_clean');
 			$this->form_validation->set_rules('candidate_name', 'Name', 'trim|required|alpha|min_length[3]|max_length[50]|xss_clean');
 			$this->form_validation->set_rules('candidate_email', 'Email ID', 'trim|required|valid_email|xss_clean|is_unique[tr_candidate_profile.candidate_email]');
-			$this->form_validation->set_rules('candidate_mobile', 'Moblie', 'trim|required|numeric|exact_length[10]|xss_clean');
+			$this->form_validation->set_rules('candidate_mobile_no', 'Moblie', 'trim|required|numeric|exact_length[10]|xss_clean');
+			$this->form_validation->set_rules('captcha_value', 'Captcha', 'trim|required|callback_validate_captcha');
 			/* Check whether registration form server side validation are valid or not */
 			if ($this->form_validation->run() == FALSE)
 	       	{
@@ -84,6 +86,8 @@ class Job_seeker extends CI_Controller {
 	       		$fb['reg_server_msg'] = 'Please provide valid information!';	
 	       		$fb['fbloginurl'] = $common->facebookloginurl();
 				$fb['institutiontype'] = $this->common_model->get_institution_type();
+				$fb['captcha'] = $this->captcha->main();
+				$this->session->set_userdata('captcha_info', $fb['captcha']);
 				$this->load->view('register-job-seekers',$fb);	
 	        }
 			else
@@ -95,6 +99,7 @@ class Job_seeker extends CI_Controller {
 					'candidate_name' => $this->input->post('candidate_name'),
 					'candidate_email' => $this->input->post('candidate_email'),
 					'candidate_mobile_no' => $this->input->post('candidate_mobile'),
+					'captcha_value' => $this->input->post('captcha_value'),
 					'candidate_password' => $common->generateStrongPassword()
 				);
 			 	/* Check whether data exist or not.exist or not condition handled in job_provider_model.php */
@@ -114,6 +119,8 @@ class Job_seeker extends CI_Controller {
 						/* mail sent success stage. send  facebook login link and server message to login page */
 						$fb['reg_server_msg'] = 'Resitration Successful!. Check your email address!!';	
 	       				$fb['fbloginurl'] = $common->facebookloginurl();
+	       				$fb['captcha'] = $this->captcha->main();
+						$this->session->set_userdata('captcha_info', $fb['captcha']);
 						$this->load->view('job-seekers-login',$fb);
 					}
 					else{
@@ -121,6 +128,8 @@ class Job_seeker extends CI_Controller {
 						$fb['reg_server_msg'] = 'Some thing wrong in mail sending process. So please register again!';	
 	       				$fb['fbloginurl'] = $common->facebookloginurl();
 						$fb['institutiontype'] = $this->common_model->get_institution_type();
+						$fb['captcha'] = $this->captcha->main();
+						$this->session->set_userdata('captcha_info', $fb['captcha']);
 						$this->load->view('register-job-seekers',$fb);
 					}
 				}
@@ -129,6 +138,8 @@ class Job_seeker extends CI_Controller {
 					$fb['reg_server_msg'] = 'Some thing wrong in data insertion process. So please register again!';	
        				$fb['fbloginurl'] = $common->facebookloginurl();
 					$fb['institutiontype'] = $this->common_model->get_institution_type();
+					$fb['captcha'] = $this->captcha->main();
+					$this->session->set_userdata('captcha_info', $fb['captcha']);
 					$this->load->view('register-job-seekers',$fb);
 				}
 				
