ac909140d7ec61dff569db879f477e30ed1c857e
kalaiR <kalamca.gs@gmail.com>
2016-11-19 18:28:37 +0530

    update admin user accounts module

--- application/controllers/admin/admin_users.php
+++ application/controllers/admin/admin_users.php
@@ -107,8 +107,120 @@ class Admin_users extends CI_Controller {
 		}
 	}
     public function user_accounts()
-	{	
-			$this->load->view('admin/user_accounts');
+	{
+		//Functionality for Both update and save
+		if(($this->input->post('action')=='update' && $this->input->post('rid')) || ($this->input->post('action')=='save')){
+				if($this->input->post('action')=='update'){
+					$id = $this->input->post('rid');
+					$validation_rules = array(
+		                            array(
+		                              'field'   => 'admin_user_name',
+		                              'label'   => 'Username',
+		                              'rules'   => 'trim|required|xss_clean|callback_edit_unique[tr_admin_users.admin_user_id.admin_user_name.'.$id.']'
+
+		                            ),
+		                            array(
+		                                 'field'   => 'admin_user_password',
+		                                 'label'   => 'Password',
+		                                 'rules'   => 'trim|required|xss_clean|'
+		                            ),
+		                            array(
+		                                 'field'   => 'admin_user_email',
+		                                 'label'   => 'User Email',
+		                                 'rules'   => 'trim|required|xss_clean|callback_edit_unique[tr_admin_users.admin_user_id.admin_user_email.'.$id.']'
+		                            ),
+		                            array(
+		                                 'field'   => 'admin_user_group',
+		                                 'label'   => 'Group',
+		                                 'rules'   => 'trim|required|xss_clean|'
+		                            ),
+		                            array(
+		                                 'field'   => 'admin_user_status',
+		                                 'label'   => 'User Status',
+		                                 'rules'   => 'trim|required|xss_clean|'
+		                            ),
+		                        );
+				}
+				else{
+					$validation_rules = array(
+	                            array(
+	                              'field'   => 'admin_user_name',
+	                              'label'   => 'Username',
+	                              'rules'   => 'trim|required|xss_clean|is_unique[tr_admin_users.admin_user_name]'
+	                            ),
+	                            array(
+	                                 'field'   => 'admin_user_password',
+	                                 'label'   => 'Password',
+	                                 'rules'   => 'trim|required|xss_clean|'
+	                            ),
+	                            array(
+	                                 'field'   => 'admin_user_email',
+	                                 'label'   => 'User Email',
+	                                 'rules'   => 'trim|required|xss_clean|callback_edit_unique[tr_admin_users.admin_user_id.admin_user_email.'.$id.']'
+	                            ),
+	                            array(
+	                                 'field'   => 'admin_user_group',
+	                                 'label'   => 'Group',
+	                                 'rules'   => 'trim|required|xss_clean|'
+	                            ),
+	                            array(
+	                                 'field'   => 'admin_user_status',
+	                                 'label'   => 'User Status',
+	                                 'rules'   => 'trim|required|xss_clean|'
+	                            ),
+	                        );
+				}
+				$this->form_validation->set_rules($validation_rules);
+		  		if ($this->form_validation->run() == FALSE) {   
+			        foreach($validation_rules as $row){
+			          $field = $row['field'];         //getting field name
+			          $error = form_error($field);    //getting error for field name
+			          //form_error() is inbuilt function
+			          //if error is their for field then only add in $errors_array array
+			          if($error){
+			            $data['status'] = strip_tags($error);
+			            $data['error'] = 1;
+			            break;
+			          }
+			        }
+		  		}
+	      		else {
+	         		$data_values = $this->admin_users_model->user_accounts($this->input->post('action')); //the value here will be pass either "update" or "save"
+	         		$data['error'] = $data_values['error'];
+			        $data['status'] = $data_values['status'];
+	     		}
+		}
+		
+    	// Delete data
+    	else if($this->input->post('action')=='delete' && $this->input->post('rid')) {
+      		$data_values = $this->admin_users_model->user_accounts('delete'); 	
+      		$data['error'] = $data_values['error'];
+		    $data['status'] = $data_values['status'];
+      	}
+      	else {
+      		$data['error'] = 0;
+		    $data['status'] = 0;
+      		$data_values = $this->admin_users_model->user_accounts('init');
+      	}
+
+		if($data['error']==1) {
+			$result['status'] = $data['status'];
+			$result['error'] = $data['error'];	
+			echo json_encode($result);
+		}
+		else if($data['error']==2) {
+			$data_ajax['user_details'] = $data_values['user_details'];
+			$data_ajax['status'] = $data['status'];
+			$result['error'] = $data['error'];
+			// print_r($data_ajax['group_values']);
+			$result['output'] = $this->load->view('admin/user_accounts',$data_ajax,true);
+			echo json_encode($result);
+		}
+		else {
+			$data['user_details'] = $data_values['user_details'];
+			$this->load->view('admin/user_accounts',$data);
+		}	
+			// $this->load->view('admin/user_accounts');
 	}
 	public function privileges()
 	{	
--- application/models/admin/admin_users_model.php
+++ application/models/admin/admin_users_model.php
@@ -51,6 +51,58 @@ class Admin_users_model extends CI_Model {
     $model_data['group_values'] = $this->db->get_where('tr_admin_user_groups')->result_array();
     return $model_data;
   }
+  // User groups - Add Edit Delete View
+  public function user_accounts($status)
+  {
+    $model_data['status'] = 0;
+    $model_data['error'] = 0;
+
+    // Update data
+    if($status=='update') {
+      $users_update_data = array( 
+                              'admin_user_name' => $this->input->post('admin_user_name'),
+                              'admin_user_password' => $this->input->post('admin_user_password'),
+                              'admin_user_email' => $this->input->post('admin_user_email'),
+                              'admin_user_group_id' => $this->input->post('admin_user_group_id'),
+                              'admin_user_status' => $this->input->post('admin_user_status')
+                            );
+      $users_update_where = '( admin_user_id="'.$this->input->post('rid').'")'; 
+      $this->db->set($users_update_data); 
+      $this->db->where($users_update_where);
+      $this->db->update("tr_admin_users", $users_update_data); 
+      $model_data['status'] = "Updated Successfully";
+      $model_data['error'] = 2;
+    }
+
+    // Save data
+    else if($status=='save') {
+      $users_insert_data = array( 
+                              'admin_user_name' => $this->input->post('admin_user_name'),
+                              'admin_user_password' => $this->input->post('admin_user_password'),
+                              'admin_user_email' => $this->input->post('admin_user_email'),
+                              'admin_user_group_id' => $this->input->post('admin_user_group_id'),
+                              'admin_user_status' => $this->input->post('admin_user_status')
+                            );
+      $this->db->insert("tr_admin_users", $users_insert_data); 
+      $model_data['status'] = "Inserted Successfully";
+      $model_data['error'] = 2;
+    }
+
+    // Delete data
+    else if($status =='delete') {
+      $users_delete_where = '(state_id="'.$this->input->post('rid').'")';
+      $this->db->delete("tr_admin_users", $users_delete_where); 
+      $model_data['status'] = "Deleted Successfully";
+      $model_data['error'] = 2;
+    }
+
+    // View
+    $this->db->select('*');
+    $this->db->from('tr_admin_users usr');
+    $this->db->join('tr_admin_user_groups grp','usr.admin_user_group_id=grp.user_group_id','inner');
+    $model_data['user_details'] = $this->db->get()->result_array();
+    return $model_data;
+  }
 }
 
 /* End of file Admin_users_model.php */
