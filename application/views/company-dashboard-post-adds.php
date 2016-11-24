cee4c077e9e4ec6ea05825c934104fc7340068c4
akiladevi <akiladevi@etekchnoservices.com>
2016-11-24 10:46:32 +0530

    updated Pro&see dashboard, post adds and custom.css

--- application/views/company-dashboard-post-adds.php
+++ application/views/company-dashboard-post-adds.php
@@ -68,34 +68,88 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-8 col-sm-8 col-xs-12">
-                        	<div class="heading-inner first-heading">
+                        <div id="post_adds" class="col-md-8 col-sm-8 col-xs-12">
+                        	 <div class="visible-lg visible-md">
+                        	 	<div class="heading-inner first-heading">
+                                	<p class="title">Adds</p>
+                            	</div>
+	                        	<h6><span>*</span>This image is <strong> illustrative</strong></h6>
+                        		<div class=" col-sm-8 col-xs-8">
+		                        	<div class="page_layout">
+		                        		<div class="header_part"></div>
+			                        	<div class="body_content">
+			                        		<div class="home_slider"></div>
+			                        		<div class="featured_jobs_area">
+			                        		 <ul>	
+			                        			<li class="fjob pull-left"></li>
+			                        			<li class="fjob pull-left"></li>
+			                        			<li class="fjob pull-left"></li>
+			                        			<li class="clearfix"> </li>
+			                        		 </ul>	
+			                        		</div>
+			                        		<div class="counter_area"> </div>
+			                        		<div class="post_adds_caurosel"></div>
+			                        	</div>	
+		                        		<div class="footer_section"></div>
+		                        		<div class="footer"> </div>	
+			                        </div>
+		                       </div> 
+		                        <div class="col-sm-4 col-xs-4">
+	                              	<h5><strong>Banner</strong></h5>
+	                              	<ul class="list-banner">
+	                              		<li><i class="fa fa-thumb-tack" aria-hidden="true"></i> Display targeted ads </li>
+	                              		<li><i class="fa fa-thumb-tack" aria-hidden="true"></i> Reach people seeking information</li>
+	                              		<li><i class="fa fa-thumb-tack" aria-hidden="true"></i> Build recognition </li>
+	                              	</ul>
+                                </div>
+	                         </div><br>
+	                         <div class="heading-inner first-heading">
                                 <p class="title">Post Adds</p>
-                            </div>
-                            <form class="form-horizontal">
-                            	<div class="form-group">
-                            		<label class="col-sm-6"> Please enter the title of your Add:</label>
-                            		<div class="col-sm-6">
-                            			<input type="text" class="form-control">
+                              </div>
+                              <div style="margin: 10px;"> </div>
+                         	<div class="profile-edit">
+                            	<form class="form-horizontal post_adds_fields">
+                            		<div class="form-group">
+                            			<div class="info" style="margin: 6px 10px;">
+                            				<i class="fa fa-info-circle" aria-hidden="true"></i> Uploading adds will be posted at the Premium adds area !
+                            			</div>
                             		</div>	
-                            	</div>
-                            	<div class="form-group">
-                            		<label class="col-sm-6">Please upload your add image:</br></label>
-                            		<div class="col-sm-6">
-                            			<input type="file" name="logo" class="uploadimage_act" />
-										<div  class="form-control imagepreview_act"></div> <br>
-										<span class="alert" style="padding-left: 0px;">( Upload image should not exceed size 400*350 )</span>
-                            		</div>
-                            		<div class="claerfix"> </div>	
-                            	</div>
-                            	<div class="form-group">
-                            		<div class="col-sm-6"> </div>
-                            		<div class="col-sm-6">
-                            			<button class="btn btn-default">SUBMIT</button>
-                            		</div>	
-                            	</div>	                            	
-                            </form>
-                       </div>
+	                            	<div class="form-group">
+	                            		<label class="col-sm-3">Title of Add :<sup class="alert">*</sup></label>
+	                            		<div class="col-sm-9">
+	                            			<input type="text" class="form-control">
+	                            		</div>	
+	                            	</div>
+	                            	<div id="provider_add_upload" class="form-group ">
+										<label class="col-sm-3">Upload Logo :<sup class="alert">*</sup></label>
+	                                    <div class="col-sm-9">
+	                                    	<div class="input-group image-preview">
+	                                        	<input id="insti_adds" type="text" class="form-control image-preview-filename uploadimage_act" placeholder="Upload add image" disabled="disabled">
+	                                        	<span class="input-group-btn">
+	                               					<button type="button" class="btn btn-default image-preview-clear" style="display:none;">
+	                                    				<span class="glyphicon glyphicon-remove"></span> Clear
+	                                        		</button>
+	                                            	<div class="btn btn-default image-preview-input">
+	                                               	 	<span class="glyphicon glyphicon-folder-open"></span>
+	                                               		<span class="image-preview-input-title">Browse</span>
+	                                                	<input class="btn_browse_adds" type="file" accept="file_extension" name="input-file-preview" />
+	                                            	</div>
+	                                        	</span>
+	                                    	</div>
+	                                    </div>	
+		                            </div>
+		                            <div class="col-sm-12 imagepreview_act pull-right"> </div>
+		                            
+		                            <div class="clearfix"> </div>
+		                            <div class="form-group">
+	                            		<div class="col-sm-6"> </div>
+	                            		<div class="col-sm-6">
+	                            			<button class="btn btn-default pull-right">SUBMIT</button>
+	                            		</div>	
+	                            	</div>	                            	
+	                            </form>
+	                        </div><br> <!--profile edit-->
+	                   </div>
                     </div>
                 </div>
             </div>
--- application/views/company-dashboard.php
+++ application/views/company-dashboard.php
@@ -3,27 +3,27 @@
 					        <div class="modal-header">
 					          <h4 class="modal-title">You are few steps away.. Kindly fill the deatils.</h4>
 					        </div>
-					        <div class="modal-body">
+					        <div class="modal-body profile-edit">					        	<form>
 					          		<div class="form-group">
-	                                    <label>Name: <span class="alert">*</span></label>
+	                                    <label>Name :<sup class="alert">*</sup></label>
 	                                    <input placeholder="" name="registrant_name" class="form-control" id="username" type="text" value="<?php echo $user_data['registrant_name']; ?>" disabled>
 	                                </div>
 	                                <div class="form-group">
-	                                    <label>Email: <span class="alert">*</span></label>
+	                                    <label>Email :<sup class="alert">*</sup></label>
 	                                    <input placeholder="" name="registrant_email_id" class="form-control" id="email" type="text" value="<?php echo $user_data['registrant_email_id']; ?>" disabled>
 	                                </div>
 	                                <div class="form-group">
-	                                    <label>Mobile: <span class="alert">*</span></label>
+	                                    <label>Mobile :<sup class="alert">*</sup></label>
 	                                    <input placeholder="" name="registrant_mobile_no" class="form-control" id="mobile" type="text">
 	                                </div>
 	                                <div class="form-group">
-	                                    <label>Password: <span class="alert">*</span></label>
+	                                    <label>Password :<sup class="alert">*</sup></label>
 	                                    <input placeholder="" name="" class="form-control" id="" type="password">
 	                                </div>
 	                                <div class="form-group">
-	                                    <label>Confirm Password: <span class="alert">*</span></label>
+	                                    <label>Confirm Password :<sup class="alert">*</sup></label>
 	                                    <input placeholder="" name="" class="form-control" id="" type="password">
-	                                </div>
+	                                </div>	                            </form>     
 					        </div>
 					        <div class="modal-footer">
 					          <button type="button" class="btn btn-default btn-block" data-dismiss="modal">Proceed</button>
