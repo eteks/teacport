1bf1cb8209482f9176f78f4ff629623f9648cd93
akiladevi <akiladevi@etekchnoservices.com>
2016-12-05 13:10:21 +0530

    created inbox popup & jobdetail pages

---
+++ application/views/company-dashboard-job-detail.php
@@ -0,0 +1,104 @@
+ <?php include('include/header.php'); ?>
+ <?php include('include/menus.php'); ?>
+        <section class="job-breadcrumb">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-sm-7 co-xs-12 text-left">
+                        <h3>Job Detail</h3>
+                    </div>
+                    <div class="col-md-6 col-sm-5 co-xs-12 text-right">
+                        <div class="bread">
+                            <ol class="breadcrumb">
+                                <li><a href="index.html">Home</a>
+                                </li>
+                                <li><a href="company-dashboard-job-detail.html">Dashboard</a>
+                                </li>
+                                <li class="active">Job Detail</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="dashboard-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12 nopadding">
+                        <div class="col-md-4 col-sm-4 col-xs-12">
+                            <div class="panel">
+                                <div class="dashboard-logo-sidebar">
+                                    <img class="img-responsive center-block" src="images/company/logo1.jpg" alt="Image">
+                                </div>
+                                <div class="text-center dashboard-logo-sidebar-title">
+                                    <h4>School Name</h4>
+                                </div>
+                            </div>
+                            <div class="profile-nav">
+                                <div class="panel">
+                                     <ul class="nav nav-pills nav-stacked">
+                                        <li>
+                                            <a href="company-dashboard.html"> <i class="fa fa-user"></i> Dashboard</a>
+                                        </li>
+                                        <li>
+                                            <a href="company-dashboard-resume.html"> <i class="fa fa-file-o"></i>Inbox </a>
+                                        </li>
+                                        <li class="active">
+                                            <a href="company-dashboard-browse-jobs.html"> <i class="fa  fa-list-ul"></i> Browse Candidate</a>
+                                        </li>
+                                        <li class="provider-jobs">
+                                            <a href="company-dashboard-post-jobs.html"> <i class="fa  fa-list-alt"></i>Jobs</a>
+                                            <ul>
+		                                        <li><a href="company-dashboard-post-jobs.html"><i class="fa  fa-list-alt"></i>New Jobs</a></li>
+		                                        <li><a href="company-dashboard-browse-jobs.html"> <i class="fa  fa-list-alt"></i>Post Jobs</a></li>
+	                                        </ul>
+	                                    </li>   
+	                                   <li>
+                                            <a href="company-dashboard-subscription.html"><i class="fa fa-money"></i> Subscribe Plan </a>
+                                        </li>
+                                        <li>
+                                            <a href="company-dashboard-feedback.html"><i class="fa fa-commenting-o"></i> Feedback </a>
+                                        </li>
+                                        <li>
+                                            <a href="company-dashboard-changepwd.html"><i class="fa fa-key" aria-hidden="true"></i> Change Password </a>
+                                        </li>
+                                     </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="company-browse-candidate" class="col-md-8 col-sm-8 col-xs-12">
+                        	<div class="heading-inner first-heading">
+                                <p class="title">Job Detail</p>
+                            </div>
+                            
+                            <div class="all-jobs-list-box2">
+                                <div class="job-box job-box-2">
+	                                <div class="col-md-2 col-sm-2 col-xs-12 hidden-xs hidden-sm">
+	                                    <div class="comp-logo">
+	                                        <a href="company-dashboard-active-jobs.html"><img src="images/company/5.png" class="img-responsive" alt="scriptsbundle"> </a>
+	                                    </div>
+	
+	                                </div>
+	                                <div class="col-md-10 col-sm-10 col-xs-12">
+	                                    <div class="job-title-box">
+	                                        <a href="company-dashboard-active-jobs.html#">
+	                                            <div class="job-title"> Technical Documentation Specialist</div>
+	                                        </a>
+	                                        <a href="company-dashboard-active-jobs.html#"><span class="comp-name">conversi Pvt. Ltd. United States </span></a>
+	                                        <a href="company-dashboard-active-jobs.html">
+	                                            <i class="fa fa-clock-o"></i> 3 Days ago	                                        </a>
+	                                    </div>
+	                                    <p>Prompta numquam mel ad, tempor definiebas id usu, cum cu feugiat bonorum. Eu pri labores maiorum patrioque, sea an tation utinam. Idque albucius prodesset ei est, sea te vide. Prompta numquam mel ad, tempor definiebas id usu, cum cu feugiat bonorum. Eu pri labores maiorum patrioque, sea an tation utinam. Idque albucius prodesset ei est, sea te vide.... </p>
+	                                </div>
+	                                <!-- <div class="job-salary">
+	                                    <i class="fa fa-money"></i> $400 - $500
+	                                </div> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+<?php include('include/footermenu.php'); ?>
+<?php include('include/footer.php'); ?>  
