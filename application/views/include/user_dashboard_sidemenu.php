d7ec0fb3574f9f4fa4c878ddf827042fd501dff9
saisasthaa <saisasthaa@gmail.com>
2016-12-08 15:02:57 +0530

    changes in seeker inbox, find jobs and applied jobs

--- application/views/include/user_dashboard_sidemenu.php
+++ application/views/include/user_dashboard_sidemenu.php
@@ -2,19 +2,19 @@
         <div class="panel">
             <ul class="nav nav-pills nav-stacked">
                 <li class="active">
-                    <a href="user-dashboard.html"> <i class="fa fa-user"></i> Profile</a>
+                    <a href="<?php echo base_url(); ?>job_seeker/dashboard"> <i class="fa fa-user"></i> Profile</a>
                 </li>
                 <li>
-                    <a href="user-dashboard-inbox.html"> <i class="fa fa-user"></i> Inbox</a>
+                    <a href="<?php echo base_url(); ?>job_seeker/inbox"> <i class="fa fa-user"></i> Inbox</a>
                 </li>
                 <li>
-                    <a href="<?php echo base_url(); ?>job_seeker/seekerdbd_editprofile"> <i class="fa fa-edit"></i> Edit Profile</a>
+                    <a href="<?php echo base_url(); ?>job_seeker/editprofile"> <i class="fa fa-edit"></i> Edit Profile</a>
                 </li>
                 <li>
-                    <a href="user-resume.html"> <i class="fa fa-file-o"></i>Find Jobs </a>
+                    <a href="<?php echo base_url(); ?>job_seeker/findjob"> <i class="fa fa-file-o"></i>Find Jobs </a>
                 </li>
                 <li>
-                    <a href="user-job-applied.html"> <i class="fa  fa-list-ul"></i>Jobs Applied</a>
+                    <a href="<?php echo base_url(); ?>job_seeker/jobsapplied"> <i class="fa  fa-list-ul"></i>Jobs Applied</a>
                 </li>
                 <li>
                     <a href="user-followed-companies.html"> <i class="fa fa-commenting-o"></i>Feedback</a>
