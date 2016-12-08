f137d0e6b8038880af30e1f561829962101daa01
kalaiR <kalamca.gs@gmail.com>
2016-12-08 16:01:36 +0530

    update social media details on end user side

--- application/config/linkedin.php
+++ application/config/linkedin.php
@@ -2,11 +2,21 @@
 /**
  * linked library configuration
  */
+//local settings
+// $config = array(
+//   'baseURL'    => APP_URL,
+//   'callbackURL'    => APP_URL.'login/linkedin',
+//   'linkedinApiKey'    => '81kkfhb074ezo8',
+//   'linkedinApiSecret' => 'fbHqfDDHctRWUgrT',
+//   'linkedinScope'  => 'r_basicprofile r_emailaddress'
+// );
+
+//server settings
 $config = array(
   'baseURL'    => APP_URL,
   'callbackURL'    => APP_URL.'login/linkedin',
-  'linkedinApiKey'    => '81kkfhb074ezo8',
-  'linkedinApiSecret' => 'fbHqfDDHctRWUgrT',
+  'linkedinApiKey'    => '81n1o6at6e35kq',
+  'linkedinApiSecret' => 'NfqbGWLdIPYKfAJC',
   'linkedinScope'  => 'r_basicprofile r_emailaddress'
 );
 ?>
