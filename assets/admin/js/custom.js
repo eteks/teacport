03e34f91520c981f4db3136a67d16118b90d45e9
akiladevi <akiladevi@etekchnoservices.com>
2016-12-27 20:44:21 +0530

    Fixed Bugs in End user & Admin

--- assets/admin/js/custom.js
+++ assets/admin/js/custom.js
@@ -788,6 +788,18 @@ $(document).ready(function(){
         return false;
      }
     });
+    
+    /* Akila Added */
+    //Job Provider Image popup for posted Ads 
+    $('.magnify-ad img').click(function () {
+	    var $img = $(this);
+	    $('#divLargerImage').html($img.clone().height(250).width(250)).add($('#divOverlay')).fadeIn();
+	});
+    $('#divLargerImage').add($('#divOverlay')).click(function () {
+	    $('#divLargerImage').add($('#divOverlay')).fadeOut(function () {
+	        $('#divLargerImage').empty();
+	    });
+	});
 
 }); // End document
 
