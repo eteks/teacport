7444596ba373a140c7d4ab2d19dd9972866bdb5d
akiladevi <akiladevi@etekchnoservices.com>
2017-01-02 13:58:40 +0530

    Updated admin & bugs in end user

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -5,12 +5,12 @@
     <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
     <!--[if IE]>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<![endif]-->
+    <![endif]-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="Teacher Recruit">
     <title>Teacher Recruit</title>
-    <link rel="icon" href="<?php echo base_url(); ?>assets/images/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="<?php echo base_url(); ?>assets/images/tr_favicon.ico" type="image/x-icon">
 
     <!-- BOOTSTRAPE STYLESHEET CSS FILES -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/bootstrap.min.css">
