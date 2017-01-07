32e4078b9287b2f4d1f3028bd2b4a3386dc1d806
spmuthu21 <muthu@globalensolutions.com>
2017-01-07 10:10:06 +0530

    issue fixed and dynamic settings initial setup worked out by muthu

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -11,13 +11,21 @@
 */
 
 //This hook 
-$hook['pre_controller'] = array(
+$hook['pre_controller'][] = array(
         'class'    => 'Common_functions',
         'function' => 'admin_global_config',
         'filename' => 'common_functions.php',
         'filepath' => 'controllers/admin',
         'params' => array()
         );
+//it is for global settings like fb,linkedin,twitter,google,paymentgateway,sms gateway and mailsettings	
+// $hook['pre_controller'][] = array(
+        // 'class'    => 'Common',
+        // 'function' => 'settings',
+        // 'filename' => 'common.php',
+        // 'filepath' => 'controllers',
+        // 'params' => array()
+        // );
 
 /* End of file hooks.php */
 /* Location: ./application/config/hooks.php */
