6b7b521415867fb42fb181190c94270cc4d8de3f
thangamariappanp <thangamgold45@gmail.com>
2017-01-28 16:44:09 +0530

    SUbscription plan Changes done

--- application/views/company-dashboard-subscription.php
+++ application/views/company-dashboard-subscription.php
@@ -136,15 +136,16 @@
 				                                <div class="plan-price text-center">
 				                                    <h4><span><i class="fa fa-inr" aria-hidden="true"></i></span>30</h4>
 				                                </div>
-				                                <div class="price-days text-center"><strong>For Next 30 days</strong></div>                               
+				                                <div class="price-days text-center"><strong>For Next 30 days</strong></div>                          
 				                                <div class="price-features text-center">
-				                                    <ul>
-															<li>Max No of vacancy Posts: 20</li>
-															<li>Sms Counts: 30</li>
-															<li>Email-count: 40</li>
+				                                    <ul class="subs_count_act">
+															<!-- <li>Max No of vacancy Posts: 20</li> -->
+															<li>Sms Counts: 30</li> 
+															<li>Email-count: 40</li> 
 															<li>Resume Download: 20</li>
-															<li>Max No of ads: 20</li>										
-													</ul>				                                    
+															<!-- <li>Max No of ads: 20</li>	 -->
+																								
+													</ul>
 				                                </div>
 				                            	</div>
 				                            	<div class=""> <a href="#" class="subs_button prem_select">Select Plan</a> </div>
@@ -160,17 +161,48 @@
 				                                    <h4><span><i class="fa fa-inr" aria-hidden="true"></i></span>30</h4>
 				                                </div>
 				                                <div class="price-days text-center"><strong>For Next 30 days</strong></div>
+				                                <input class="subs_input_val_upgact1" type="text" disabled="disabled" />
+				                                <input class="subs_input_val_upgact2" type="text" disabled="disabled" />
+				                                <input class="subs_input_val_upgact3" type="text" disabled="disabled" />
 				                                <div class="price-features text-center">
-				                                    <ul>
-															<li>Max No of vacancy Posts: 20</li>
-															<li>Sms Counts: 30</li>
-															<li>Email-count: 40</li>
-															<li>Resume Download: 20</li>
-															<li>Max No of ads: 20</li>
+				                                    <ul class="subs_count_act">
+															<!-- <li>Max No of vacancy Posts: 20</li> -->
+															<li id="subs_list_upg_sms" class="subs_list_act" data-toggle="subs_tooltip" title="Click Here To Edit Plan!">Sms Counts: 30<i class="icon-edit subs_edit"></i></li>
+															<div id="popover-form" class="hide popover_form_upg_sms">
+										            <form>
+										            <div class="form-group">
+										            <label>SMS COUNT</label>
+										                <input id="popover-value" type="text" placeholder="Enter Value" class="form-control popover_value_upg_sms" maxlength="5" />
+										            </div>
+										            <button type="button" class="btn btn-success popover_upg_sms_save">Save</button>
+										            </form>
+										        </div>
+															<li id="subs_list_upg_email" class="subs_list_act">Email-count: 40<i class="icon-edit subs_edit"></i></li>
+															<div id="popover-form" class="hide popover_form_upg_email">
+										            <form>
+										            <label>EMAIL COUNT</label>
+										            <div class="form-group">
+										                <input id="popover-value" type="text" placeholder="Enter Value" class="form-control popover_value_upg_email" maxlength="5" />
+										            </div>
+										            <button type="button" class="btn btn-success popover_upg_email_save">Save</button>
+										            </form>
+										        </div>
+															<li id="subs_list_upg_resume" class="subs_list_act">Resume Download: 20<i class="icon-edit subs_edit" data-original-title="" title=""></i></li>
+															<div id="popover-form" class="hide popover_form_upg_resume">
+										            <form>
+										            <div class="form-group">
+										            <label>RESUME COUNT</label>
+										                <input id="popover-value" type="text" placeholder="Enter Value" class="form-control popover_value_upg_resume" maxlength="5" />
+										            </div>
+										            <button type="button" class="btn btn-success popover_save popover_upg_resume_save">Save</button>
+										            </form>
+										        </div>
+															<!-- <li>Max No of ads: 20</li> -->
 													</ul>                                   
 				                                </div>
 				                            </div>
 				                            <div class=""> <a href="#" class="subs_button upg_select">Select Plan</a> </div>
+				                            <!-- <div> <a href="#" class="subs_button renew_select">Reset Plan</a> </div> -->
 				                          </div>
 				                        </div>
 				                        <div class="col-md-4 col-sm-6 col-xs-12">
@@ -182,15 +214,26 @@
 				                                <div class="plan-price text-center">
 				                                    <h4><span><i class="fa fa-inr" aria-hidden="true"></i></span>30</h4>
 				                                </div>
-				                                <div class="price-days text-center"><strong>For Next 30 days</strong></div>                               
+				                                <div class="price-days text-center"><strong>For Next 30 days</strong></div>                            
 				                                <div class="price-features text-center">
-				                                    <ul>
-															<li>Max No of vacancy Posts: 20</li>
-															<li>Sms Counts: 30</li>
-															<li>Email-count: 40</li>
-															<li>Resume Download: 20</li>
-															<li>Max No of ads: 20</li>										
-													</ul>				                                    
+				                                    <ul class="subs_count_act">
+															<!-- <li>Max No of vacancy Posts: 20</li> -->
+															
+															<li> Sms Counts: 30
+															</li>
+															<div id="popover-form" class="hide popover_form_renewal_sms">
+										            <form>
+										            <div class="form-group">
+										                <input id="popover-value" type="text" placeholder="Enter Value" class="form-control popover_value_renewal_sms" maxlength="5" />
+										            </div>
+										            <button type="button" class="btn btn-success popover_renewal_sms_save">Save</button>
+										            </form>
+										        	</div>
+													<li>
+													Email-count: 40</li>
+													<li>Resume Download: 20</li>
+													<!-- <li>Max No of ads: 20</li>-->
+													</ul>                    
 				                                </div>
 				                            	</div>
 				                            	<div class=""> <a href="#" class="subs_button renew_select">Select Plan</a> </div>
